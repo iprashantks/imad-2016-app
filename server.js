9e144d596a832e16b3e55dc64868d07f7942fd16
iprashantks <iprashantks@gmail.com>
2016-10-26 15:33:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -101,6 +101,7 @@ app.get('/test-db', function (req, res) {
 });
 
 app.get('/:articleName',function(req, res) {
+    var articleName = req.param.articleName;
     res.send(createTemplate(articles[articleName]));
 });
 

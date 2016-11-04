5c61b34a6e7422cd24d1b1df43be6563ae54c7a7
iprashantks <iprashantks@gmail.com>
2016-11-04 15:02:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/', function (req, res) {
+app.get('/login', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'login.html'));
 });
 
